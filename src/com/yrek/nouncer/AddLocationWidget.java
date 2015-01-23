b701649f4a206501590dbc306ff1d66ecfed7898
Q.P.Liu <qpliu@yahoo.com>
2015-01-23 14:07:52 -0800

    Show nearby locations in add location widget.

--- src/com/yrek/nouncer/AddLocationWidget.java
+++ src/com/yrek/nouncer/AddLocationWidget.java
@@ -1,17 +1,25 @@
 package com.yrek.nouncer;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.yrek.nouncer.data.Location;
+import com.yrek.nouncer.processor.PointProcessor;
 
 class AddLocationWidget extends Widget {
+    private final ArrayAdapter<Location> nearbyLocations;
+    private final double[] nearbyCenter = new double[2];
     private OnFinish onFinish;
 
     AddLocationWidget(final Main activity, int id) {
         super(activity, id);
+        nearbyLocations = nearbyLocationsAdapter();
         view.findViewById(R.id.add_button).setOnClickListener(onClickListener);
         view.findViewById(R.id.cancel_button).setOnClickListener(onClickListener);
+        ((AdapterView) view.findViewById(R.id.nearby_location_list)).setAdapter(nearbyLocations);
     }
 
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -78,6 +86,8 @@ class AddLocationWidget extends Widget {
         view.findViewById(R.id.longitude).setEnabled(true);
         ((TextView) view.findViewById(R.id.elevation)).setText("");
         view.findViewById(R.id.elevation).setEnabled(true);
+        view.findViewById(R.id.nearby_locations).setVisibility(View.GONE);
+        nearbyLocations.clear();
     }
 
     void show(OnFinish onFinish, String name, double latitude, double longitude, double elevation) {
@@ -90,5 +100,29 @@ class AddLocationWidget extends Widget {
         view.findViewById(R.id.longitude).setEnabled(false);
         ((TextView) view.findViewById(R.id.elevation)).setText(String.valueOf(elevation*3.28084));
         view.findViewById(R.id.elevation).setEnabled(false);
+        nearbyCenter[0] = latitude;
+        nearbyCenter[1] = longitude;
+        nearbyLocations.clear();
+        nearbyLocations.addAll(activity.store.getLocationStore().getLocations(latitude, longitude, 500.0));
+        if (nearbyLocations.getCount() == 0) {
+            view.findViewById(R.id.nearby_locations).setVisibility(View.GONE);
+        } else {
+            view.findViewById(R.id.nearby_locations).setVisibility(View.VISIBLE);
+        }
+    }
+
+    private ArrayAdapter<Location> nearbyLocationsAdapter() {
+        return new ArrayAdapter<Location>(activity, R.layout.nearby_location_list_entry) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = activity.getLayoutInflater().inflate(R.layout.nearby_location_list_entry, parent, false);
+                }
+                Location item = getItem(position);
+                ((TextView) convertView.findViewById(R.id.name)).setText(item.getName());
+                ((TextView) convertView.findViewById(R.id.distance)).setText(String.format("%.2fmi %s", PointProcessor.distance(nearbyCenter[0], nearbyCenter[1], item.getLatitude(), item.getLongitude())*0.000621371, TrackListWidget.headingName(PointProcessor.heading(nearbyCenter[0], nearbyCenter[1], item.getLatitude(), item.getLongitude()))));
+                return convertView;
+            }
+        };
     }
 }
